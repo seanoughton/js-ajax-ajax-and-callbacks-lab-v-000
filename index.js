9ab237fe0a27ddccf5bafc55f9e5ba37dbff2d94
Sean Oughton <seanoughton@gmail.com>
2018-07-06 00:26:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 $(document).ready(function (){
 });
+
+var url = "https://api.github.com/repositories"
+
+//Include repository name, description, and a link to the HTML URL. Also include repository owner login, repository owner avatar as an image, and a link to the owner's profile page. Hint: Pay close attention to the structure of the search results!
+function searchRepositories() {
+  searchTerm = document.getElementById("searchTerms").value;
+  const src = document.getElementById("repository-template").innerHTML;
+  const template = Handlebars.compile(src);
+  $.get(url).done(function(data) {
+    //document.getElementById("results").innerHTML = data
+    console.log(data);
+    const repoList = template(data);
+    document.getElementById("results").innerHTML = repoList
+  });
+
+};
