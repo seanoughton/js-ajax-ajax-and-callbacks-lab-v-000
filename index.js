7d3fb3c0fe26f18b4b79554bd3a76cc11a08b770
Sean Oughton <seanoughton@gmail.com>
2018-07-06 17:55:58 +0000

    Done.

--- index.js
+++ index.js
@@ -19,25 +19,30 @@ function searchRepositories() {
     //console.log(data.items);
     const repoList = template(data.items);
     document.getElementById("results").innerHTML = repoList
+  }).fail(function displayError(error){
+    //what happens when an error occurs
+    let response = "<p>I'm sorry, there's been an error. Please try again.</p>";
+    document.getElementById("errors").innerHTML = response;
   });
 };
 
 //that gets the repository's commits from the GitHub API and display them in the details div. For each commit, list the SHA, the author, the author's login, and the author's avatar as an image.
   function showCommits(el) {
-    const commitUrl = "https://api.github.com/repos/"
+    const baseUrl = "https://api.github.com/repos/"
     //get the repo's commits from GitHub API
       //need the repo url for the api to get the commits
       //ex: https://api.github.com/repos/rails/rails/commits
     const name = el.dataset.name
+    const commitUrl = baseUrl + name + '/' + name + '/commits'
     //console.log(commitUrl + name + '/' + name + '/commits');
 
     const src = document.getElementById("commit-template").innerHTML;
     const template = Handlebars.compile(src);
     //dispay the commits in the details div
     //for each commit, list : SHA, author, author login, author avatar image
-    $.get(commitURl).done(function(data) {
+    $.get(commitUrl).done(function(data) {
       //document.getElementById("results").innerHTML = data
-      console.log(data.items);
+      //console.log(data);
       const commitData = template(data);
       document.getElementById("details").innerHTML = commitData;
     });
