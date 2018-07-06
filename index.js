6d9497f43e9d57ccca0f10d099d43c161db259ea
Sean Oughton <seanoughton@gmail.com>
2018-07-06 20:41:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function searchRepositories() {
   function showCommits(el) {
     const baseUrl = "https://api.github.com/repos/"
     const name = el.dataset.name
-    const commitUrl = baseUrl + name + '/commits'
+    const commitUrl = baseUrl + name + 'repo/commits'
     //const commitUrl = baseUrl + name + '/' + name + '/commits'
     ///https://api.github.com/repos/owner/repo/commits/
     //console.log(commitUrl)
