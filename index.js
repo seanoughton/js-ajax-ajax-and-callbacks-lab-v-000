568f9bf2da2407c055d091eaa4b2eeea6721231c
Sean Oughton <seanoughton@gmail.com>
2018-07-06 20:06:52 +0000

    Done.

--- index.js
+++ index.js
@@ -12,12 +12,26 @@ function searchRepositories() {
   searchURl = url + searchTerm
   $.get(searchURl).done(function(data) {
     //const repoList = template(data.items);
+    const repos = data.items
+    console.log(repos);
+    //iterate through the array and create html for the elements you want to show
+    const repoList = '<ul>' + repos.map(r => {
+     return (`
+            <li>
+              <h2><a href="${r.html_url}">${r.name}</a></h2>
+              <p>Description: ${r.description}</p>
+              <p>Owner: ${r.owner.login}</p>
+              <img src="${r.owner.avatar_url}}" height="32" width="32">
+              <h2><a href="${r.html_url}">${r.owner.html_url}</a></h2>
+              <a href="#" data-name="${r.name}" onclick="showCommits(this)">Show Commits</a>
+            </li>`
+            )
+    }).join('') + "</ul>"
 
 
 
 
-
-    //document.getElementById("results").innerHTML = repoList
+    document.getElementById("results").innerHTML = repoList
   }).fail(function displayError(error){
     let response = "<p>I'm sorry, there's been an error. Please try again.</p>";
     document.getElementById("errors").innerHTML = response;
@@ -34,11 +48,25 @@ function searchRepositories() {
     const name = el.dataset.name
     const commitUrl = baseUrl + name + '/' + name + '/commits'
 
-    const src = document.getElementById("commit-template").innerHTML;
-    const template = Handlebars.compile(src);
+//handlebars stuff
+    //const src = document.getElementById("commit-template").innerHTML;
+    //const template = Handlebars.compile(src);
+
+
 
     $.get(commitUrl).done(function(data) {
-      const commitData = template(data);
+      //const commitData = template(data);
+      const commits = data
+      const commitData = '<ul>' + commits.map(r => {
+       return (`
+              <li>
+                <p>${r.sha}</p>
+                <p>${r.author.login}</p>
+                <img src="${r.author.avatar_url}" height="32" width="32">
+              </li>`
+              )
+      }).join('') + "</ul>"
+
       document.getElementById("details").innerHTML = commitData;
     });
   };
